ff91322b74c22678f54b7b2ffcb75f25104d2833
Fermin <feeralvarez013@gmail.com>
2022-05-02 23:28:32 -0300

    GetBooks modificado, autores distintos parametros

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -1,28 +1,50 @@
 const database = require('../database');
 
 const getBooks = async(req, res ) => {
-    const response = 
-    await database.query('SELECT books."ISBN", books.name, publisher, total_pages, published_at, image_link, categories.name as category, string_agg(authors.name::character varying, \', \') as Authors FROM (books JOIN categories ON books.category = categories.id JOIN written_by ON written_by."ISBN" = books."ISBN" JOIN authors On written_by."Author" = authors.id) GROUP BY books."ISBN", categories.name');
+    const responseBooks = 
+    await database.query('SELECT books."ISBN", books.name, publisher, total_pages, published_at, image_link, categories.name as category FROM (books JOIN categories ON books.category = categories.id JOIN written_by ON written_by."ISBN" = books."ISBN" JOIN authors On written_by."Author" = authors.id) GROUP BY books."ISBN", categories.name');
 
-    if(response.rows.length>0)
-        res.status(200).json(response.rows);
+    const responseAuthors = 
+    await database.query('SELECT written_by."ISBN", authors.name FROM  (authors JOIN written_by ON written_by."Author" = authors."id" JOIN books On written_by."ISBN" = books."ISBN")');
+
+    compactAuthors(responseBooks, responseAuthors)
+
+    if(responseBooks.rows.length>0)
+        res.status(200).json(responseBooks.rows);
     else
         res.status(404).json({error: 'Not Found'});
 }
 
 const getBookByISBN = async(req, res) => {
     if(!isNaN(req.params.ISBN)){
-        const response = 
+        const responseBooks = 
         await database.query('SELECT "ISBN", books.name, publisher, total_pages, published_at, image_link, categories.name as category FROM (books JOIN categories ON books.category = categories.id) WHERE "ISBN" = $1', [req.params.ISBN]);
 
-        if(response.rows.length > 0){
-            res.status(200).json(response.rows[0]);
+        if(responseBooks.rows.length > 0){
+            res.status(200).json(responseBooks.rows[0]);
         }else{
             res.status(404).json({error: 'Not Found'});
         }
     }else{
         res.status(400).json({error: 'Invalid parameter'});
     }    
+}
+
+function compactAuthors(responseBooks, responseAuthors){
+    let books = responseBooks.rows;
+    let authors = responseAuthors.rows;
+    let aux = {};
+    for(e in books){
+        let count = 0;
+        books[e]["authors"] = aux;
+        for(i in authors){
+            if(authors[i].ISBN == books[e].ISBN){
+                aux["author"+count++] = authors[i].name;
+            }
+        }
+        console.log(aux);
+    }
+
 }
 
 module.exports = {
