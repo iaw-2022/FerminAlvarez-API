7dc3e5103eb0694eff7207f096f2b5e4f9b8bb90
Fermin Alvarez <feeralvarez013@gmail.com>
2022-05-14 17:50:00 -0300

    Extracción de datos para la api de scrapping

---
+++ src/scrapping-settings.js
@@ -0,0 +1,9 @@
+let bookshopmapping = [
+    {name: 'libreriadonquijote', id :[1]},
+    {name: 'cuspide', id : [2,3]},
+    {name: 'buscalibre', id : [4]},
+    {name: 'tematika', id : [5]}]
+
+module.exports = {
+    bookshopmapping
+}
