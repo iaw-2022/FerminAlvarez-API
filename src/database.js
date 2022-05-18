4ac12b38e94adced113c6dcb8146cbb293e0c86c
Fermin Alvarez <feeralvarez013@gmail.com>
2022-05-18 09:22:22 -0300

    Extraída configuración a variables de entorno

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+const config = require('./config');
 
 const pool = new Pool ({
-    host: 'localhost',
-    user: 'postgres',
-    password: '1234',
-    database: 'testdb',
-    port: '5432'
+    host: config.DB_HOST,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: config.DB_DATABASE,
+    port: config.DB_PORT,
 })
 
 module.exports = pool;
