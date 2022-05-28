f64439d812fdcbc5585f2a5513623ac37a1869e9
Fermin <feeralvarez013@gmail.com>
2022-05-28 15:58:19 -0300

    Configuración para deploy

--- src/config.js
+++ src/config.js
@@ -10,5 +10,6 @@ module.exports = {
     AUTH0_JWKSURI: process.env.AUTH0_JWKSURI,
     AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
     AUTH0_ISSUER: process.env.AUTH0_ISSUER,
-    AUTH0_ALGORITHM: process.env.AUTH0_ALGORITHM
+    AUTH0_ALGORITHM: process.env.AUTH0_ALGORITHM,
+    ssl: { rejectUnauthorized: false }
 }
