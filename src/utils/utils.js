b2a24fff63d5a75b8663df85d7c4829fd168b2d9
Fermin <feeralvarez013@gmail.com>
2022-06-06 23:02:47 -0300

    Solucion compact authors

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,24 @@
 function compactAuthors(responseBooks, responseAuthors){
     let books = responseBooks.rows;
     let authors = responseAuthors.rows;
+    let result = []
     for(e in books){
         let aux = [];
         books[e]["authors"] = aux;
+        authorFounded = false
         for(i in authors){
             if(authors[i].ISBN == books[e].ISBN){
+                authorFounded = true
                 aux.push({
                     "id": authors[i].id,
                     "name": authors[i].name
                 });
             }
         }
+        if(authorFounded)
+            result.push(books[e])
     }
+    responseBooks.rows = result;
 }
 
 module.exports = {
